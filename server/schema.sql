4bef3b9a40c5257c3af2327bcba2c0fc27f16821
Mallow <linlam0923@gmail.com>
2023-01-17 23:51:38 -0500

    save progress made on schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -4,8 +4,27 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(20),
+  messageText VARCHAR(100),
+  roomName VARCHAR(10),
+  -- FOREIGN KEY(roomName) REFERENCES rooms(roomName)
 );
 
+CREATE TABLE rooms (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  roomName VARCHAR(10),
+  messageData SET(),
+  -- FOREIGN KEY(roomName) REFERENCES messages(roomName)
+)
+
+create TABLE friends (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(20),
+  friendsList SET(),
+  -- FOREIGN KEY(username) REFERENCES messages(username)
+
+)
 /* Create other tables and define schemas for them here! */
 
 
