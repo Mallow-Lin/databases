2e4c0599bc4f9957b0cccbcc402720bc1f4a4afd
Mallow <linlam0923@gmail.com>
2023-01-18 20:29:20 -0500

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,34 +2,32 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(20),
-  messageText VARCHAR(100),
-  roomName VARCHAR(10),
-  -- FOREIGN KEY(roomName) REFERENCES rooms(roomName)
+CREATE TABLE rooms (
+  id INT NOT NULL AUTO_INCREMENT,
+  roomName varchar(10),
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE rooms (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  roomName VARCHAR(10),
-  messageData SET(),
-  -- FOREIGN KEY(roomName) REFERENCES messages(roomName)
-)
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  username varchar(20),
+  PRIMARY KEY(id)
+);
 
-create TABLE friends (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(20),
-  friendsList SET(),
-  -- FOREIGN KEY(username) REFERENCES messages(username)
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  username varchar(20),
+  messageText varchar(100),
+  roomID INT NOT NULL,
+  userID INT NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(roomID) REFERENCES rooms(id),
+  FOREIGN KEY(userID) REFERENCES users(id)
+);
 
-)
+
 /* Create other tables and define schemas for them here! */
-
-
-
-
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
