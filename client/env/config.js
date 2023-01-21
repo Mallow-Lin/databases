7c74c9330c41f66e3846b1a3600878b9e680634a
Mallow <linlam0923@gmail.com>
2023-01-20 20:51:11 -0500

    save progress made

---
+++ client/env/config.js
@@ -0,0 +1,6 @@
+$.ajaxPrefilter(function (settings, _, jqXHR) {
+  jqXHR.setRequestHeader('Authorization', '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');
+});
+
+// Put your campus prefix here
+window.CAMPUS = 'rpp';
